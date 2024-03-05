0a4a039f548dd58ba54470c19d5e50f39a3f4d54
MorenoSegoviaSamuel <morenosegoviaSamuel@gmail.com>
2024-03-05 17:01:52 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ej2dit5/fragments/Fragment_home.java
@@ -0,0 +1,26 @@
+package com.example.ej2dit5.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ej2dit5.R;
+
+
+
+
+public class Fragment_home extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+
+}
